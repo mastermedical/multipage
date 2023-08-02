fcb2315abd9e044bccb4f033e36af549569c1386
mastermedical <140692007+mastermedical@users.noreply.github.com>
2023-08-02 12:57:44 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -23,7 +23,7 @@
   }
 
   // Fetch the flashcard data from the JSON file on GitHub
-  fetch('https://raw.githubusercontent.com/mastermedical/flash/main/flashcardswithkeywords.json')
+  fetch('https://raw.githubusercontent.com/mastermedical/multipage/main/flashcardswithkeywords.json')
     .then((response) => response.json())
     .then((data) => {
       flashcardData = data; // Store the fetched data in the flashcardData variable
